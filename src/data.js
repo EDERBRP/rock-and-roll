4e8cfa18bf147e0b0103449fbe5275854de63001
EDERBRP <116520010+EDERBRP@users.noreply.github.com>
2022-11-21 12:16:02 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -15,7 +15,7 @@ function chillHop() {
         {
             name:"SHE Radio Rock And Roll Channel",
             artist: "Rock And Roll",
-            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/SHE-Radio-Rock-And-Roll-Channel-1.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/SHE-Radio-Rock-And-Roll-Channel.png",
             id: uuidv4(),
             color: ["#D7B0D5", "#CBDFE0"],
             audio: "http://80.85.84.114:8054/stream",
