316c94ecbb5e4226103472d46e25a4f38085199f
EDERBRP <116520010+EDERBRP@users.noreply.github.com>
2022-11-21 10:04:28 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ function chillHop() {
         {
             name:"Rock And Roll Heaven",
             artist: "Rock And Roll",
-            cover: "https://png.pngtree.com/png-vector/20190726/ourlarge/pngtree-modern-square-cool-music-for-party-lights-png-image_1629696.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Rock-And-Roll-Heaven-1.jpg",
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
             audio: "https://streams.radio.co/see9cf0008/listen",
@@ -15,7 +15,7 @@ function chillHop() {
         {
             name:"SHE Radio Rock And Roll Channel",
             artist: "Rock And Roll",
-            cover: "https://png.pngtree.com/png-vector/20190726/ourlarge/pngtree-modern-square-cool-music-for-party-lights-png-image_1629696.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/SHE-Radio-Rock-And-Roll-Channel-1.jpg",
             id: uuidv4(),
             color: ["#D7B0D5", "#CBDFE0"],
             audio: "http://80.85.84.114:8054/stream",
@@ -27,13 +27,13 @@ function chillHop() {
             cover: "https://iconoderadio.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
-            audio: "https://playerservices.streamtheworld.com/api/livestream-redirect/SP_R2375983.mp3?dist=onlineradiobox",
+            audio: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Studio-Rock-ZQ-1.jpg",
             active: false,
         },
         {
             name:"Radio QDX",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Radio-QDX-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://radio.streemlion.com:2795/stream",
@@ -42,7 +42,7 @@ function chillHop() {
         {
             name:"WDGY",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/WDGY-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://ice41.securenetsystems.net/WDGY?playSessionID=A7BBFB31-B0A9-6D6B-E796145345569DDF",
@@ -51,7 +51,7 @@ function chillHop() {
         {
             name:"Hot Wax Radio",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Hot-FM-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://stream.abyss-shoutcast.com/proxy/hotwaxradiocom/stream",
@@ -60,7 +60,7 @@ function chillHop() {
         {
             name:"KPPC 96.9 FM",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/KPPC-96.9-FM-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://ice1.securenetsystems.net/DEMOSTN?playSessionID=A7D39A93-A19B-1599-56A435707A3E0C4F",
@@ -69,7 +69,7 @@ function chillHop() {
         {
             name:"102.7 WIRL",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/102.7-WIRL-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://17853.live.streamtheworld.com/WIRLAMAAC_SC?dist=onlineradiobox",
@@ -78,7 +78,7 @@ function chillHop() {
         {
             name:"Puregold Rock 'N' Roll",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Puregold-Rock-N-Roll-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "http://69.162.103.66:8001/stream",
@@ -87,7 +87,7 @@ function chillHop() {
         {
             name:"Rock n' Roll Radio",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Rock-n-Roll-Radio-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://digitalaudiobroadcasting.net/8006/stream",
@@ -96,7 +96,7 @@ function chillHop() {
         {
             name:"Max 93.5 FM",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Max-93.5-FM-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://crystalout.surfernetwork.com:8001/KMKX-FM_MP3",
@@ -105,7 +105,7 @@ function chillHop() {
         {
             name:"Too Much Too Soon Radio",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Too-Much-Too-Soon-Radio-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://ais-edge37-live365-dal02.cdnstream.com/a15209",
@@ -114,7 +114,7 @@ function chillHop() {
         {
             name:"KZEL 96.1 FM",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/KZEL-96.1-FM-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://playerservices.streamtheworld.com/api/livestream-redirect/KZELFM.mp3?dist=onlineradiobox",
@@ -123,7 +123,7 @@ function chillHop() {
         {
             name:"North Central PA's Z-FM",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/North-Central-PAs-Z-FM-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://stream-50.zeno.fm/tsme2ttc2hhvv?zs=JOD29nDSSja7pyjM4TA6lQ",
@@ -132,7 +132,7 @@ function chillHop() {
         {
             name:"181.FM Rock 40 (Rock & Roll)",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/181.FM-Rock-40-Rock-Roll-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://listen.181fm.com/181-rock40_128k.mp3",
@@ -141,7 +141,7 @@ function chillHop() {
         {
             name:"Radio Free CLE",
             artist: "Rock And Roll",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Radio-Free-CLE-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "http://radio.dawgtalkers.net:8000/listenAAA",
@@ -153,13 +153,13 @@ function chillHop() {
             cover: "https://iconoderadio.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
-            audio: "https://us9.maindigitalstream.com/ssl/WHVO",
+            audio: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Oldies-Radio-1.jpg",
             active: false,
         },
         {
             name:"Rock 'N' Roll Oldies Radio",
             artist: "Kids",
-            cover: "https://iconoderadio.jpg",
+            cover: "http://rock-and-roll.dab-radios.org/ro/wp-content/uploads/2022/11/Rock-N-Roll-Oldies-Radio-1.jpg",
             id: uuidv4(),
             color: ["#008781", "#cfc08c"],
             audio: "https://ais-edge51-live365-dal02.cdnstream.com/a63650",
